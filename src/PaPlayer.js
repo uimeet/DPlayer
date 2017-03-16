f82e8d260161ed2b802a71f210d68f738e2564cf
joyber <youxianbo@ling1.org>
2017-03-16 21:17:23 +0800

    switchClarity 去掉快照

--- src/PaPlayer.js
+++ src/PaPlayer.js
@@ -1590,21 +1590,21 @@ class PaPlayer {
      * @param start_time
      */
     switchClarity(video) {
-        let start_time = this.video.currentTime, poster;
-        const canvas = document.createElement("canvas");
-        canvas.width = this.video.videoWidth;
-        canvas.height = this.video.videoHeight;
-        canvas.getContext('2d').drawImage(this.video, 0, 0, canvas.width, canvas.height);
-        poster = canvas.toDataURL("image/png");
+        let start_time = this.video.currentTime, poster="";
 
-        this.pause();
+        // const canvas = document.createElement("canvas");
+        // canvas.width = this.video.videoWidth;
+        // canvas.height = this.video.videoHeight;
+        // canvas.getContext('2d').drawImage(this.video, 0, 0, canvas.width, canvas.height);
+        // poster = canvas.toDataURL("image/png");
+
         this.option.video.url = this.video.src = video.url;
-
-        this.video.poster = poster;
+        // this.video.poster = poster;
         this.video.currentTime = 0;
         if (start_time > 0) {
             this.setStartTime(start_time);
         }
+        this.pause();
         this.TestMediaType();
         this.setClarity(video.clarity, video.current_clarity);
     }
